106f1de357da9f08631b93ec98cfb056d28460b6
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-04-08 20:22:19 +0000

    Output Practice

--- src/output/Output.java
+++ src/output/Output.java
@@ -1,10 +1,21 @@
 package output;
 
+import java.util.Locale;
+
 public class Output {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Teaching
+		double x = 10.35784;
+		String name = "Rodrigo";
+		int age = 31;
+		double salary = 4000.0;
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%s tem %d anos e ganha US$ %.2f %n", name, age, salary);
+		
 	}
 
 }
---
+++ src/output/OutputPractice.java
@@ -0,0 +1,26 @@
+package output;
+
+public class OutputPractice {
+
+	public static void main(String[] args) {
+		/*
+		  	String product1 = "Computer";
+			String product2 = "Office desk";
+			int age = 30;
+			int code = 5290;
+			char gender = 'F';
+			double price1 = 2100.0;
+			double price2 = 650.50;
+			double measure = 53.234567;
+		  	Products:
+			Computer, which price is $ 2100,00
+			Office desk, which price is $ 650,50
+			Record: 30 years old, code 5290 and gender: F
+			Measue with eight decimal places: 53,23456700
+			Rouded (three decimal places): 53,235
+			US decimal point: 53.235
+		 * */
+		
+		
+	}
+}

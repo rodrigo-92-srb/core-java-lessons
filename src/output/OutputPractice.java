9491a65c40ef3c95ee122a2a014c4c2a65ad2e19
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-04-09 11:47:55 +0000

    Output Practice done!

--- src/output/OutputPractice.java
+++ src/output/OutputPractice.java
@@ -1,5 +1,7 @@
 package output;
 
+import java.util.Locale;
+
 public class OutputPractice {
 
 	public static void main(String[] args) {
@@ -16,11 +18,27 @@ public class OutputPractice {
 			Computer, which price is $ 2100,00
 			Office desk, which price is $ 650,50
 			Record: 30 years old, code 5290 and gender: F
-			Measue with eight decimal places: 53,23456700
-			Rouded (three decimal places): 53,235
+			Measure with eight decimal places: 53,23456700
+			Rounded (three decimal places): 53,235
 			US decimal point: 53.235
 		 * */
-		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $%.2f%n", product1, price1);
+		System.out.printf("%s, which price is $%.2f%n", product2, price2);
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.printf("Measure with eight decimal places: %.8f %n", measure);
+		Locale.setDefault(Locale.GERMAN);
+		System.out.printf("Rounded (three decimal places): %.3f %n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
 		
 	}
 }

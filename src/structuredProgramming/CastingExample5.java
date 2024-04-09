c95f3cd884cec569020e40feb93300d613c5c8db
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-04-09 13:17:52 +0000

    Casting Examples

---
+++ src/structuredProgramming/CastingExample5.java
@@ -0,0 +1,14 @@
+package structuredProgramming;
+
+public class CastingExample5 {
+
+	public static void main(String[] args) {
+		double a;
+		int b;
+		a = 5.0;
+		b = (int) a;
+		System.out.println(b);
+
+	}
+
+}
